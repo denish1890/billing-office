60247bd72f628c89fc05df9e8b0dc9f49229fe5e
denish1890 <rakholiyadenish@gmail.com>
2026-03-10 21:52:18 +0530

    Update office1.py

--- office1.py
+++ office1.py
@@ -11,9 +11,9 @@ from streamlit_autorefresh import st_autorefresh
 # ==========================
 # 🔹 Initialize Firebase
 # ==========================
-cred = credentials.Certificate('billing-bv-firebase-adminsdk-fbsvc-a54b087e56')  
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred)
+cred = credentials.Certificate(st.secrets["firebase"])
+firebase_admin.initialize_app(cred)
+    
 
 # ==========================
 # 🔹 MySQL Connection Function
@@ -532,5 +532,6 @@ with tabs[4]:
     # Display saved QR
     if row and row['upi_qr_image_founder']:
         st.image(row['upi_qr_image_founder'], caption="Saved Founder UPI QR", width=150)
+
 
 
